2dd2b5d390de1110b3497c0d69a6f3a3fd6a6e40
hitohira <hitohirasakuno@gmail.com>
2019-11-23 21:20:56 +0900

    size change

--- MM25/job.sh
+++ MM25/job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #PBS -q TODO
-#PBS select=4:ncpus=32:mpiprocs=16:ompthreads=2
+#PBS select=8:ncpus=32:mpiprocs=32:ompthreads=0
 #PBS -W group_list=TODO
 #PBS -l walltime=00:30:00
 #PBS -o result.txt
 #PBS -e err.txt
 
 cd $PBS_O_WORKDIR
-mpiexec -np 128 ./main
+mpiexec -np 256 ./main
 
